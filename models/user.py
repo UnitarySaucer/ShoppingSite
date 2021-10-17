1359d0cc8748e152c9051c7846e0409932d55af8
Reinel Parrado <reinelparrado@gmail.com>
2021-10-17 17:31:29 -0500

    created user model

---
+++ models/user.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from models.db import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password_digest = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow(
+    ), nullable=False, onupdate=datetime.utcnow)
+
+    def __init__(self, username, email, password_digest):
+        self.username = username
+        self.email = email
+        self.password_digest = password_digest
