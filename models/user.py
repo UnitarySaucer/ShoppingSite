0badca1aa7db72d68f533b977edb4acc6824a1a8
Reinel Parrado <reinelparrado@gmail.com>
2021-10-17 18:51:52 -0500

    added methods

--- models/user.py
+++ models/user.py
@@ -17,3 +17,20 @@ class User(db.Model):
         self.username = username
         self.email = email
         self.password_digest = password_digest
+
+    def json(self):
+        return {"id": self.id, "username": self.username, "email": self.email, "created_at": str(self.created_at), "updated_at": str(self.updated_at)}
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    @classmethod
+    def find_all(cls):
+        return User.query.all()
+
+    @classmethod
+    def find_by_id(cls, user_id):
+        user = User.query.filter_by(id=user_id).first()
+        return user
