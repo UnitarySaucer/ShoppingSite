a207faa2b207b18cb2e3e326827828d4c479a573
Reinel Parrado <reinelparrado@gmail.com>
2021-10-18 04:18:39 -0500

    imported and styled

---
+++ app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <NavBar/>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
