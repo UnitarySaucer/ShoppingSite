6be398ec805d66ce37f657b2784911d9d1872d5a
Reinel Parrado <reinelparrado@gmail.com>
2021-10-18 10:37:25 -0500

    added routes

--- app/src/router.js
+++ app/src/router.js
@@ -1,10 +1,12 @@
 import VueRouter from 'vue-router'
 import Home from './pages/Home.vue'
 import LogInRegister from './pages/LogInRegister'
+import Product from './pages/Product'
 
 const routes = [
   { path: '/', component: Home, name: 'Home' },
-  { path: '/login-reg', component: LogInRegister, name: 'LogInRegister' }
+  { path: '/login-reg', component: LogInRegister, name: 'LogInRegister' },
+  { path: '/product/:id', component: Product, name: 'Product' }
 ]
 
 export default new VueRouter({
