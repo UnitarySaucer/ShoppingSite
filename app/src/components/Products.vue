dc50b825cf5d85c638422b44b924b5b16dd5aa13
Reinel Parrado <reinelparrado@gmail.com>
2021-10-18 06:57:26 -0500

    added component

--- app/src/components/Products.vue
+++ app/src/components/Products.vue
@@ -1,13 +1,34 @@
 <template>
-  
+  <div class="product-box">
+    <div class="product" v-for="product in products" :key="product.id" >
+      <p>{{product.name}}</p>
+      <p>${{product.price}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import {BASE_URL} from '../globals'
+import axios from 'axios'
 export default {
-
+  name: 'Products',
+  data: () =>({
+    products: []
+  }),
+  methods: {
+    async getProducts(){
+      const res = await axios.get(`${BASE_URL}/api/products`)
+      console.log(res.data)
+      this.products = res.data
+    }
+  },
+  mounted:function(){
+    this.getProducts()
+  }
 }
 </script>
 
 <style>
+
 
 </style>
