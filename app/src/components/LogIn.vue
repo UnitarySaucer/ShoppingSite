9083a01fda53abb8d3ac7426d9ff7279c0211246
Reinel Parrado <reinelparrado@gmail.com>
2021-10-18 08:21:38 -0500

    created log in component form

---
+++ app/src/components/LogIn.vue
@@ -0,0 +1,37 @@
+<template>
+    <form class="login" @submit.prevent="handleSubmit">
+      <div>
+        <label>Username:</label>
+        <input type="text" placeholder="Username" v-model="username" />
+        <label>Password:</label>
+        <input type="password" placeholder="Password" v-model="password" />
+        <button type="submit">Log In</button>
+      </div>
+    </form>
+</template>
+
+<script>
+import {BASE_URL} from '../globals'
+import axios from 'axios'
+
+export default {
+  name: "LogIn",
+  data: () => ({
+    username: '',
+    password: ''
+  }),
+  methods: {
+    async handleSubmit(){
+      const res = await axios.post(`${BASE_URL}/auth/login`,{
+        username: this.username,
+        password: this.password
+      })
+      localStorage.setItem('token', res.data.token)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
