65447ea5968d8c4b5338bd4b92f13ca7646cb6a1
Reinel Parrado <reinelparrado@gmail.com>
2021-10-18 04:17:49 -0500

    imported product resources

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from models.product import Product
 from models.review import Review
 from resources.auth import Login, Register
 from resources.Users import UserList, IndividualUser
+from resources.Reviews import ReviewList, IndividualReview
+from resources.Products import ProductList, IndividualProduct
 
 app = Flask(__name__)
 
@@ -21,8 +23,15 @@ migrate = Migrate(app, db)
 
 api.add_resource(Login, '/auth/login')
 api.add_resource(Register, '/auth/register')
+
 api.add_resource(UserList, '/api/users')
 api.add_resource(IndividualUser, '/api/users/<int:id>')
+
+api.add_resource(ReviewList, '/api/reviews')
+api.add_resource(IndividualReview, '/api/reviews/<int:id>')
+
+api.add_resource(ProductList, '/api/products')
+api.add_resource(IndividualProduct, '/api/products/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
