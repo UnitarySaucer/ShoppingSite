506cc79ed7f2a091c7d408afe667c3985825c58a
Reinel Parrado <reinelparrado@gmail.com>
2021-10-17 21:54:06 -0500

    added to auth.py login

--- resources/auth.py
+++ resources/auth.py
@@ -6,17 +6,34 @@ from middleware import create_token, gen_password, strip_token, read_token, comp
 
 class Login(Resource):
     def post(self):
-        pass
+        data = request.get_json()
+        try:
+            user = User.find_user(email=data['email']).json()
+            check_pw = compare_password(
+                data['password'], user['password_digest'])
+            if check_pw:
+                payload = {
+                    'id': user['id'],
+                    'username': user['username'],
+                    'email': user['email']
+                }
+                token = create_token(payload)
+                return {'user': payload, 'token': token}, 200
+            else:
+                return {'msg': "Please Check Your Credentials"}, 404
+        except:
+            return {'msg': 'Error'}, 404
 
     def get(self):
-        pass
+        token = strip_token(request)
+        return read_token(token)
 
 
 class Register(Resource):
     def post(self):
         data = request.get_json()
         params = {
-            "name": data['name'],
+            "username": data['username'],
             "email": data['email'],
             "password_digest": gen_password(data['password'])
         }
