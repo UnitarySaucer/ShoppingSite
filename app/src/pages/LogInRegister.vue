2fef92cbe0427fccf4b4df49296e44c38251ad05
Reinel Parrado <reinelparrado@gmail.com>
2021-10-18 08:35:32 -0500

    imported log in and register components

--- app/src/pages/LogInRegister.vue
+++ app/src/pages/LogInRegister.vue
@@ -1,10 +1,19 @@
 <template>
-  
+  <div>
+    <LogIn />
+    <Register />
+  </div>
 </template>
 
 <script>
+import LogIn from '../components/LogIn.vue'
+import Register from '../components/Register.vue'
 export default {
-
+  name: "LogInRegister",
+  components: {
+    LogIn,
+    Register
+  }
 }
 </script>
 
